17ae103870de999d07a7171e2a35cc255f34d7ca
Ghaitsa Tsuraya Shofa <ghaitsatsurayashofa@gmail.com>
2025-12-08 11:03:15 +0700

    Nim_array

--- python/Nim_Array.py
+++ python/Nim_Array.py
@@ -1,15 +1,18 @@
-nim = input("Masukkan NIM")  
+nim = "2510651"  
 
+# Membuat array digit
 digits = []
 for d in nim:
     digits.append(int(d))
 
+# Perhitungan
 total = sum(digits)
 maksimum = max(digits)
 minimum = min(digits)
 rata_rata = total / len(digits)
 reverse_array = digits[::-1]
 
+# Output
 print("Array digit        :", digits)
 print("Total digit        :", total)
 print("Digit maksimum     :", maksimum)
